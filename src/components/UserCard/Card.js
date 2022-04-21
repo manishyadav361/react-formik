27c27b8f6e74a8cfd106f1fbc48211addef30a87
manishyadav361 <manishyadav361my@gmail.com>
2022-04-21 14:06:35 +0530

    added the user card component

---
+++ src/components/UserCard/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+  return (
+    <section>
+      <h3>Manish Yadav</h3>
+      <p>Vasai,Mumbai</p>
+      <button>view more</button>
+    </section>
+  );
+}
+
+export default Card;
